31768725dc9377f987d0b4f105d62bbb72f50684
Rossimac <rossmac@gmail.com>
2025-02-22 18:23:45 +0000

    Added WaitFor in AppHost to wait on dependencies

--- src/AppHost/Program.cs
+++ src/AppHost/Program.cs
@@ -5,14 +5,17 @@ var clusteringTable = storage.AddTables("clustering");
 var grainStorage = storage.AddBlobs("grain-state");
 
 var orleans = builder.AddOrleans("default")
+    .WaitFor(storage)
     .WithClustering(clusteringTable)
     .WithGrainStorage("Default", grainStorage);
 
-builder.AddProject<Projects.Silo>("silo")
+var silo = builder.AddProject<Projects.Silo>("silo")
+    .WaitFor(storage)
     .WithReference(orleans)
     .WithReplicas(1);
 
 builder.AddProject<Projects.Client>("frontend")
+    .WaitFor(silo)
     .WithReference(orleans.AsClient())
     .WithExternalHttpEndpoints()
     .WithReplicas(1);
